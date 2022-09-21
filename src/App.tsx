7a47c2aeb17d560ed931628536498d6f3c8a8d73
Ani <ani.tarverdyan.87@gmail.com>
2022-09-21 15:38:01 +0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"
+
 import './App.css';
 
+
+
+const Http = new XMLHttpRequest();
+const url='https://api.thecatapi.com/v1/categories';
+Http.open("GET", url);
+Http.send();
+
+/* Http.onreadystatechange = (e) => {
+  console.log(JSON.parse(Http.responseText), 'hi')
+} */
+
+
+
+/* const b=fetch('https://api.thecatapi.com/v1/categories')
+  .then((response) => response.json())
+  .then((data) => console.log(data)); */
 function App() {
+/*   const [category, setCategory]=useState();
+ */  const location = useLocation();
+  console.log(location, 'location');
+
+  const handleCategoryClick= ()=>{
+/* setCategory()
+ */  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {/*   <Routes>
+    
+        
+      </Routes> */}
+   <div className='ctegory-wrapper'> {JSON.parse(Http.responseText).map((item:any)=>{return <div onClick={handleCategoryClick} className='category'>{item.name}</div>})}</div>
     </div>
   );
 }
